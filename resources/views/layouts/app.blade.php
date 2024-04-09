8ae6abf164cccd45f87bcb87524ec4476b910d72
fermamud <e2395117@cmaisonneuve.qc.ca>
2024-04-09 16:41:02 -0400

    Ajouter header
    Implémentation de l'en-tête basée sur la charte graphique. Avec des questions à vérifier en groupe et en attente d'approbation.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,30 @@
 
 </head>
 <body>
+<header>
+    <div class="div-header">
+            <div class="line-1"></div>
+            <div class="line-2"></div>
+            <div class="div-nav">
+                <img src="{{asset('assets/img/svg/logo.svg')}}" alt="">
+                <div class="div-list-nav">
+                    <nav>
+                        <ul>
+                            <li><a href="" class="active">List of cars</a></li>
+                            <li><a href="">About us</a></li>
+                            <li><a href="">Sales Policies</a></li>
+                        </ul>
+                    </nav>
+                </div>           
+            </div>
+            <div class="div-lang">
+                <button class="btn btn-primaire">FR</button>
+                <div class="div-lien">
+                    <a href="">Sign in</a>
+                </div>
+            </div>
+        </div>
+    </header>
     @yield('content')
 </body>
 </html>
