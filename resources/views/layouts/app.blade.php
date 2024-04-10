4daa09a4ca3f4fdab9f42dc8fcca2868c9953b5c
Frata Mamud <e2395117@cmaisonneuve.qc.ca>
2024-04-10 15:01:45 -0400

    Affichage header complet
    Avec les traductions.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,44 +10,49 @@
 
 </head>
 <body>
-<header>
-    <div class="div-header">
+    @php $locale = session()->get('locale') @endphp
+    <header>
+        <div class="div-header">
             <div class="line-1"></div>
             <div class="line-2"></div>
             <div class="div-nav">
                 <img src="{{asset('assets/img/svg/logo.svg')}}" alt="logo">
+                @auth
                 <div class="div-list-nav">
                     <nav>
                         <ul class="list-nav">
-                            <li><a href="">Employe</a></li>
-                            <li><a href="">Client</a></li>
-                            <li class="voiture-nav"><a href="" class="active">Voiture</a>                          
+                            <li><a href="">@lang('Employee')</a></li>
+                            <li><a href="">@lang('Client')</a></li>
+                            <li class="voiture-nav"><a href="" class="active">@lang('Car')</a>                          
                                 <ul class="sous-list-nav">
-                                    <li><a href="" class="active">Liste des voitures</a></li>
-                                    <li><a href="">Ajouter une voiture</a></li>
-                                    <li><a href="">Paramètres voitures</a></li>
+                                    <li><a href="" class="active">@lang('Cars list')</a></li>
+                                    <li><a href="">@lang('Add car')</a></li>
+                                    <li><a href="">@lang('Car parameters')</a></li>
                                 </ul>
                             </li>
                         </ul>
                     </nav>
                 </div>           
+                @endauth
+                <!-- version sans sous-liste / guest -->
+                <div class="div-list-nav">
+                    <nav>
+                        <ul class="list-nav">
+                            <li><a href="" class="active">@lang('Cars list')</a></li>
+                            <li><a href="">@lang('About us')</a></li>
+                            <li><a href="">@lang('Sales Policies')</a></li>
+                        </ul>
+                    </nav>
+                </div> 
             </div>
-
-            <!-- version sans sous-liste / guest -->
-            <!-- <div class="div-list-nav">
-                <nav>
-                    <ul>
-                        <li><a href="" class="active">List of cars</a></li>
-                        <li><a href="">About us</a></li>
-                        <li><a href="">Sales Policies</a></li>
-                    </ul>
-                </nav>
-            </div>  -->
-
             <div class="div-lang">
-                <button class="btn btn-primaire">FR</button>
+                @if ($locale == 'en')
+                    <button class="btn btn-primaire"><a href="{{ route('lang', 'fr') }}">FR</a></button>
+                @else ($locale == 'fr')
+                    <button class="btn btn-primaire"><a href="{{ route('lang', 'en') }}">EN</a></button>
+                @endif
                 <div class="div-connexion">
-                    <a href="">Sign in</a>
+                    <a href="">@lang('Sign in')</a>
                 </div>
             </div>
         </div>
