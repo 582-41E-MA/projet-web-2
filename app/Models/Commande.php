b3e91c4f701eb0911d27f4cc184bd0b980cb9cbf
2296789 <wuxvying@gmail.com>
2024-04-26 11:34:44 -0400

    page 404

--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -10,13 +10,15 @@ class Commande extends Model
     use HasFactory;
     protected $fillable = [
     'id',
-    'user_id',
     'voiture_id',
+    'user_id',
     'payment_id',
     'statut_id',
     'expedition_id',
     'date',
     'quantite',
-    'prix'
+    'prix',
+    'create_at',
+    'update_at'
     ];
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Page Teste')
+@section('content')
+   
+<main class="wrapper">
+welcome
+</main>
+@endsection
