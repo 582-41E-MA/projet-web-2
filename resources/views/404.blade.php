e252cf11fa35f9eafd74fe4038bf8262d6948f30
2296789 <wuxvying@gmail.com>
2024-05-01 10:42:19 -0400

    francais page 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -5,7 +5,7 @@
 <main class="wrapper">
     <div class="page404">
         <img src="{{asset('assets/img/svg/404.svg')}}" alt="404">
-        <h3>Oops ! Page not found !</h3>
+        <h3>@lang('Oops ! Page not found !')</h3>
     </div>
 </main>
 @endsection
